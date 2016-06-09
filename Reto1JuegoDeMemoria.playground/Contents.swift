f46bd496ee08e8e3db4c7c734b44cca358d7f996
tomalic <tomalic@hotmail.com>
2016-06-09 23:28:07 +0200

    Version 2

--- Reto1JuegoDeMemoria.playground/Contents.swift
+++ Reto1JuegoDeMemoria.playground/Contents.swift
@@ -7,17 +7,29 @@ var numero = 1...100
 
 for num in numero{
     
-    if num >= 30 && num <= 40 {
-        print (num, "Viva Swift!!")
+    if num >= 30 && num <= 40 && num%2==0 && num%5==0 {
+        print (num, "Par, Bingo!!, Viva Swift!!")
     }
-    else if num%5==0 {
-        print (num, "Bingo!!")
+    else if num >= 30 && num <= 40 && num%2>0 && num%5==0 {
+        print (num, "Impar, Bingo!!, Viva Swift!!")
+    }
+    else if num >= 30 && num <= 40 && num%2>0 {
+        print (num, "Impar, Viva Swift!!")
+    }
+    else if num >= 30 && num <= 40 && num%2==0 {
+        print (num, "Par, Viva Swift!!")
+    }
+    else if num%5==0 && num%2==0 {
+        print (num, "Par, Bingo!!")
+    }
+    else if num%5==0 && num%2>0 {
+        print (num, "Impar, Bingo!!")
     }
     else if num%2==0 {
         print (num, "Par")
-        }
+    }
     else {
-            print (num, "Impar")
+        print (num, "Impar")
         }
     }
 
